a2e93d22045e55df25ae948e31ae09657b239752
Lia Duggan <liaduggan@MD-DJ-4.fritz.box>
2021-01-01 18:01:53 +0000

    first commit solo

---
+++ example.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class example {
+    public static void main(String[] args){
+        System.out.println();
+        String message = "messageType";
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
