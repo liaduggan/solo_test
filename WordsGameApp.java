04cc4568efb66301fd52385883707a19eb01e5dd
Lia Duggan <liaduggan@MD-DJ-4.fritz.box>
2021-01-06 19:05:27 +0000

    adding app class first commit

---
+++ WordsGameApp.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class WordsGameApp {
+    public static void main(String[] args){
+        System.out.println();
+        String message = "messageType";
+        JOptionPane.showMessageDialog(null, message);
+        //adding changes uptream here
+        //adding new changes in editor
+        
+    }
+}
